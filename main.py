4002284ff6d509eb7fe393737a384dbe2c1979d1
aizimpamvu <hallyiizza200@gmail.com>
2022-11-01 00:00:23 +0200

    Updated test module

--- main.py
+++ main.py
@@ -18,53 +18,64 @@ trainings = ["Amahugurwa y\'abahinzi ba Kawa",
 trainings_description = ["Ubusobanuro 1 ",
                          "Ubusobanuro 2",
                          "Ubusobanuro 3"]
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-driver.get("https://staging.smartkungahara.rw/#/login")
 
 
 # Get and set email
+class Training:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://staging.smartkungahara.rw/#/login")
+    def __init__(self):
 
-email = driver.find_element(By.XPATH, '/html/body/app-root/app-login/div/div/div/div[2]/div/div/form/div[1]/input')
+        email = self.driver.find_element(By.XPATH,
+                                         '/html/body/app-root/app-login/div/div/div/div[2]/div/div/form/div[1]/input')
 
-email.send_keys(EMAIL)
-time.sleep(3)
-# Enter password
-password = driver.find_element(By.XPATH, '/html/body/app-root/app-login/div/div/div/div[2]/div/div/form/div[2]/input')
-password.send_keys(PASS)
-time.sleep(3)
-password.send_keys(Keys.ENTER)
-time.sleep(5)
-# Training module menu
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/app-asidenavbar/aside/section/ul/li[7]/a').click()
-time.sleep(2)
-driver.find_element(By.XPATH,
-                    '/html/body/app-root/app-admin/div/app-asidenavbar/aside/section/ul/li[7]/ul/li/a').click()
-time.sleep(2)
-# Create training button
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-list/h3/div/div[2]/a').click()
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
-                              '1]/div/div/div/div[1]/div[1]/div/input').send_keys(random.choice(trainings))
-time.sleep(3)
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
-                              '1]/div/div/div/div[1]/div[2]/div/input').send_keys(random.choice(trainings_description))
-time.sleep(3)
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
-                              '1]/div/div/div/div[2]/div[1]/div/ng-multiselect-dropdown/div/div[1]/span').click()
-time.sleep(3)
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
-                              '1]/div/div/div/div[2]/div[1]/div/ng-multiselect-dropdown/div/div[2]/ul[1]/li/input').send_keys(
-    "Not Applied")
-time.sleep(3)
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
-                              '1]/div/div/div/div[2]/div[1]/div/ng-multiselect-dropdown/div/div[2]/ul[2]/li['
-                              '1]/div').click()
+        email.send_keys(EMAIL)
+        time.sleep(3)
+        # Enter password
+        password = self.driver.find_element(By.XPATH,
+                                            '/html/body/app-root/app-login/div/div/div/div[2]/div/div/form/div[2]/input')
+        password.send_keys(PASS)
+        time.sleep(3)
+        password.send_keys(Keys.ENTER)
+        time.sleep(5)
+    def addTraining(self):
+        self.driver.find_element(By.XPATH,
+                            '/html/body/app-root/app-admin/div/app-asidenavbar/aside/section/ul/li[7]/a').click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH,
+                            '/html/body/app-root/app-admin/div/app-asidenavbar/aside/section/ul/li[7]/ul/li/a').click()
+        time.sleep(2)
+        # Create training button
+        self.driver.find_element(By.XPATH,
+                            '/html/body/app-root/app-admin/div/div/section/app-training-list/h3/div/div[2]/a').click()
+        self.driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
+                                      '1]/div/div/div/div[1]/div[1]/div/input').send_keys(random.choice(trainings))
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
+                                      '1]/div/div/div/div[1]/div[2]/div/input').send_keys(
+            random.choice(trainings_description))
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
+                                      '1]/div/div/div/div[2]/div[1]/div/ng-multiselect-dropdown/div/div[1]/span').click()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
+                                      '1]/div/div/div/div[2]/div[1]/div/ng-multiselect-dropdown/div/div[2]/ul[1]/li/input').send_keys(
+            "Not Applied")
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div['
+                                      '1]/div/div/div/div[2]/div[1]/div/ng-multiselect-dropdown/div/div[2]/ul[2]/li['
+                                      '1]/div').click()
 
-time.sleep(3)
-driver.find_element(By.XPATH, '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div[2]/button[2]').click()
-time.sleep(3)
-driver.maximize_window()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH,
+                            '/html/body/app-root/app-admin/div/div/section/app-training-create/form/div[2]/button[2]').click()
+        time.sleep(3)
+        self.driver.maximize_window()
 
-driver.find_element(By.XPATH, '/html/body/ngb-modal-window/div/div/div[3]/div/div[3]/button/span').click()
-print(driver.current_url)
-time.sleep(2)
+        self.driver.find_element(By.XPATH, '/html/body/ngb-modal-window/div/div/div[3]/div/div[3]/button/span').click()
+        print(self.driver.current_url)
+        time.sleep(2)
+
+
+Training()
+
