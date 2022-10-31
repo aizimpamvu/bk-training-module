bad6fa8864a5d4df97f5c62e8c65041cfad31664
aizimpamvu <hallyiizza200@gmail.com>
2022-10-31 16:50:03 +0200

    Login to the system

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
+from  selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 #
-EMAIL = "hallyizza200@gmail.com"
-PASS = "123454321"
+EMAIL = "admind@tamanta.net"
+PASS = "Dd@1234567890"
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://www.facebook.com/")
+driver.get("https://staging.smartkungahara.rw/")
 
 # Get and set email
-email = driver.find_element(By.ID, "email")
+email = driver.find_element(By.XPATH, '/html/body/app-root/app-login/div/div/div/div[2]/div/div/form/div[1]/input')
 time.sleep(3)
 email.send_keys(EMAIL)
-time.sleep(3)
 
-# Get and set password
-
-password = driver.find_element(By.ID, "pass")
+# Enter password
+password = driver.find_element(By.XPATH, '/html/body/app-root/app-login/div/div/div/div[2]/div/div/form/div[2]/input')
 password.send_keys(PASS)
 
-time.sleep(3)
-
 password.send_keys(Keys.ENTER)
-login = driver.find_element(By.TAG_NAME, 'button')
-print(login.text)
-login.click()
-
 time.sleep(3)
 
 
